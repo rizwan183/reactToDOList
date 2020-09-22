92e1b47af84dbf91da323d90cd1cab4c49c95d81
rizwan183 <r.ansari12996@gmail.com>
2020-09-23 00:38:43 +0530

    fromLable

--- todo-app/src/FormControl.js
+++ todo-app/src/FormControl.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from '@material-ui/core';
-function App() {
+import { Button ,FormControl,InputLabel,Input} from '@material-ui/core';
+function FormControl1() {
 
   const [todos,setTodos] = useState([1,2,3,4,5,6,7,8,9,10]); 
   const [input,setInput] = useState("");
@@ -20,7 +20,10 @@ function App() {
       <h1> Hello World </h1>
 
       <form>
-          <input value={input} onChange={event => setInput(event.target.value)}/>
+      		<FormControl>
+				  <InputLabel htmlFor="my-input">Write Your Todo</InputLabel>
+				  <Input value={input} onChange={event => setInput(event.target.value)} />
+			</FormControl>
           <Button disabled={!input} type='submit' onClick={addTodo} variant="contained" color="primary">
               Add To-Do
           </Button>
@@ -41,5 +44,4 @@ function App() {
     </div>
   );
 }
-
-export default App;
+export default FormControl1;
--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import FormControl1 from './FormControl';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FormControl1 />
   </React.StrictMode>,
   document.getElementById('root')
 );
